7a4652d07e9a86bfe5c63f56a3cefa7c45a02623
alokverma <alokverma749@gmail.com>
2025-04-10 15:34:21 +0530

    fix: cors

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", process.env.FRONTENDDOMAIN],
+    origin: ["http://localhost:5173", process.env.VITE_FRONTENDDOMAIN],
     credentials: true,
   })
 );
